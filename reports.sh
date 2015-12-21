2306f57995a2b871c0c64250344c9f4f123aed61
Justin Rittenhouse <jrittenh@nd.edu>
2015-12-21 16:40:25 -0500

    Updated parent shell script to use a variable for script patsh and call all scripts using that variable

--- reports.sh
+++ reports.sh
@@ -2,20 +2,22 @@
 
 #echo "RUN JOB"
 
-export PERL5LIB=/home/jrittenh/aleph/scripts/modules
+export RPT_PATH=/home/jrittenh/aleph/scripts/annex
+
+export PERL5LIB=$RPT_PATH
 #~ /home/jrittenh/aleph/scripts/annex/shelfTrayCount.pl -e jrittenh@nd.edu
 
 
 
 if [ `date +%H` = "08" ]; then
-    /home/jrittenh/aleph/scripts/annex/shelfTrayCount.pl -e jrittenh@nd.edu,abales@nd.edu,tmorton@nd.edu,michael@professionalsystems.co,miranda.r.vannevel.7@nd.edu,swalton@nd.edu
-    #~ /home/jrittenh/aleph/scripts/annex/workerStats.pl -e jrittenh@nd.edu -d `date +%Y%m%d -d yesterday`
-    #~ /home/jrittenh/aleph/scripts/annex/workerStats.pl -e jrittenh@nd.edu,abales@nd.edu,tmorton@nd.edu,michael@professionalsystems.co,miranda.r.vannevel.7@nd.edu,swalton@nd.edu -d `date +%Y%m%d -d yesterday`
-    #~ /home/jrittenh/aleph/scripts/annex/workerStats.pl -e jrittenh@nd.edu,abales@nd.edu,tmorton@nd.edu,michael@professionalsystems.co,miranda.r.vannevel.7@nd.edu,swalton@nd.edu -d `date +%Y%m%d -d yesterday` -h
-    /home/jrittenh/aleph/scripts/annex/shelfFillRpt.pl -e jrittenh@nd.edu,abales@nd.edu,tmorton@nd.edu,michael@professionalsystems.co,miranda.r.vannevel.7@nd.edu,swalton@nd.edu
-    #~ /home/jrittenh/aleph/scripts/annex/ingestTime.pl
+    $RPT_PATH/shelfTrayCount.pl -e jrittenh@nd.edu,abales@nd.edu,tmorton@nd.edu,michael@professionalsystems.co,miranda.r.vannevel.7@nd.edu,swalton@nd.edu
+    #~ $RPT_PATH/workerStats.pl -e jrittenh@nd.edu -d `date +%Y%m%d -d yesterday`
+    #~ $RPT_PATH/workerStats.pl -e jrittenh@nd.edu,abales@nd.edu,tmorton@nd.edu,michael@professionalsystems.co,miranda.r.vannevel.7@nd.edu,swalton@nd.edu -d `date +%Y%m%d -d yesterday`
+    #~ $RPT_PATH/workerStats.pl -e jrittenh@nd.edu,abales@nd.edu,tmorton@nd.edu,michael@professionalsystems.co,miranda.r.vannevel.7@nd.edu,swalton@nd.edu -d `date +%Y%m%d -d yesterday` -h
+    $RPT_PATH/shelfFillRpt.pl -e jrittenh@nd.edu,abales@nd.edu,tmorton@nd.edu,michael@professionalsystems.co,miranda.r.vannevel.7@nd.edu,swalton@nd.edu
+    #~ $RPT_PATH/ingestTime.pl
     if [ `date +%w` = "1" ]; then
-        /home/jrittenh/aleph/scripts/annex/fillRequestRpt.pl -e jrittenh@nd.edu,abales@nd.edu,tmorton@nd.edu,miranda.r.vannevel.7@nd.edu
-        /home/jrittenh/aleph/scripts/annex/fillRequestRpt.pl -f 2015-08-24 -u `date +%Y-%m-%d -d "last sunday"` -e jrittenh@nd.edu,abales@nd.edu,tmorton@nd.edu,miranda.r.vannevel.7@nd.edu
+        $RPT_PATH/fillRequestRpt.pl -e jrittenh@nd.edu,abales@nd.edu,tmorton@nd.edu,miranda.r.vannevel.7@nd.edu
+        $RPT_PATH/fillRequestRpt.pl -f 2015-08-24 -u `date +%Y-%m-%d -d "last sunday"` -e jrittenh@nd.edu,abales@nd.edu,tmorton@nd.edu,miranda.r.vannevel.7@nd.edu
     fi
 fi
