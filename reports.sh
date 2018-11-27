b0cc3710dfe4277b1cbcdd45d253e6796dfa3e85
Justin Rittenhouse <jrittenh@nd.edu>
2018-11-27 09:31:16 -0500

    Remove disabled jobs

--- reports.sh
+++ reports.sh
@@ -8,15 +8,7 @@ cd $RPT_PATH
 
 if [ `date +%H` = "08" ]; then
     # Daily Jobs
-    ## Ingest Jobs/Jobs with Hallet copied on message
-    #~ $RPT_PATH/shelfTrayCount.pl -e annex-reports-list@nd.edu,michael@professionalsystems.co
-    #~ $RPT_PATH/workerStats.pl -e annex-reports-list@nd.edu,michael@professionalsystems.co -d `date +%Y%m%d -d yesterday`
-    #~ $RPT_PATH/workerStats.pl -e annex-reports-list@nd.edu,michael@professionalsystems.co -d `date +%Y%m%d -d yesterday` -h
-    #~ $RPT_PATH/shelfFillRpt.pl -e annex-reports-list@nd.edu,michael@professionalsystems.co
-    #~ $RPT_PATH/ingestTime.pl
-
     ## Regular Jobs
-    #~ $RPT_PATH/workerStats.pl -e jrittenh@nd.edu -d `date +%Y%m%d -d yesterday`
     $RPT_PATH/workerStats.pl -e annex-reports-list@nd.edu -d `date +%Y%m%d -d yesterday`
 
     if [ `date +%w` = "1" ]; then
@@ -26,10 +18,4 @@ if [ `date +%H` = "08" ]; then
         $RPT_PATH/shelfTrayCount.pl -e annex-reports-list@nd.edu
         $RPT_PATH/shelfFillRpt.pl -e annex-reports-list@nd.edu
     fi
-fi
-
-if [ `date +%H` = "18" ]; then
-    echo ""
-    #~ $RPT_PATH/workerStats.pl -e annex-reports-list@nd.edu,michael@professionalsystems.co -d `date +%Y%m%d -d today`
-    #~ $RPT_PATH/workerStats.pl -e annex-reports-list@nd.edu,michael@professionalsystems.co -d `date +%Y%m%d -d today` -h
 fi
