5a96f70e9f9e0b6574e9a75757a09e9ede6698c3
Justin Rittenhouse <jrittenh@nd.edu>
2019-04-01 16:32:56 -0400

    Add additional width and height parameters to query for updated fill stats

--- shelfFillRpt.pl
+++ shelfFillRpt.pl
@@ -31,18 +31,27 @@ GetOptions(
 );
 $emails = $test ? 'jrittenh@nd.edu' : ($emails ? $emails : 'jrittenh@nd.edu');
 
+my %widths = (
+    'A' => 140,
+    'B' => 140,
+    'C' => 140,
+    'D' => 140,
+    'E' => 92,
+);
+my $threshold = .9;
+
 # A-D = 18 inches
 # E = 12 inches
 # Subtract .5 inches for cardboard thickness to get max item width
 
-my $annexSQL = "SELECT shelf, size, tray, COUNT(item) AS \"items\", SUM(thickness) AS \"width\", SUM(thickness) / 8::float AS \"inches\", AVG(thickness) AS \"avg\", MAX(thickness) AS \"max\", MIN(thickness) AS \"min\" ".
+my $annexSQL = "SELECT shelf, size, twidth, theight, tray, COUNT(item) AS \"items\", SUM(thickness) AS \"width\", SUM(thickness) / 8::float AS \"inches\", AVG(thickness) AS \"avg\", MAX(thickness) AS \"max\", MIN(thickness) AS \"min\" ".
     "FROM ( ".
-        "SELECT shelves.barcode AS \"shelf\", shelves.size AS \"size\", trays.barcode AS \"tray\", items.barcode AS \"item\", items.thickness ".
+        "SELECT shelves.barcode AS \"shelf\", shelves.size AS \"size\", LEFT(shelves.size, 1) AS \"twidth\", RIGHT(shelves.size, 1) AS \"theight\", trays.barcode AS \"tray\", items.barcode AS \"item\", items.thickness ".
         "FROM shelves INNER JOIN trays ON shelves.id = trays.shelf_id ".
         #~ "INNER JOIN items ON trays.id = items.tray_id ".
         "LEFT JOIN items ON trays.id = items.tray_id ".
     ") stocked_items ".
-    "GROUP BY shelf, size, tray ".
+    "GROUP BY shelf, size, tray, twidth, theight ".
     "";
 
 my $data = annexQuery($annexSQL);
@@ -73,35 +82,33 @@ $table .= "    </tr>\n";
 
 $etable = $table;
 
-foreach my $t (sort {$a->{'shelf'} cmp $b->{'shelf'} || $a->{'tray'} cmp $b->{'tray'}} @$data) {
-    my %tray = %$t;
-    
-    next if ($tray{'tray'} =~ /^TRAY-$tray{'shelf'}/);
-    if ($tray{'width'} < ($tray{'size'} =~ /^E/ ? 69 : 105)) {
-        if ($tray{'shelf'} =~ /^SHELF-\w-\d{3}-\w$/ && $tray{'tray'} =~ /^TRAY-\w\w\d{5}$/) {
-            $rpt .= join(' | ', (sprintf('%-13s', $tray{'shelf'}), sprintf('%-4s', $tray{'size'}), sprintf('%-12s', $tray{'tray'}), sprintf('%5d', $tray{'items'}), sprintf('%5d', $tray{'width'}), sprintf('%7.2f', 100 * ($tray{'width'} / ($tray{'size'} =~ /^E/ ? 92 : 140)))))."\n";
-            #~ $table .= "    <tr>\n".join ('', map("        <td>$_</td>\n", ($tray{'shelf'}, $tray{'size'}, $tray{'tray'}, $tray{'items'}, $tray{'inches'}, 100 * ($tray{'width'} / ($tray{'size'} =~ /^E/ ? 92 : 140)))))."    \n</tr>\n";
+foreach my $tray (sort {$a->{'shelf'} cmp $b->{'shelf'} || $a->{'tray'} cmp $b->{'tray'}} @$data) {
+    next if ($tray->{'tray'} =~ /^TRAY-$tray->{'shelf'}/);
+    if ($tray->{'width'} < int(($widths{$tray->{'twidth'}} * $threshold) + .5)) {
+        if ($tray->{'shelf'} =~ /^SHELF-\w-\d{3}-\w$/ && $tray->{'tray'} =~ /^TRAY-\w\w\d{5}$/) {
+            $rpt .= join(' | ', (sprintf('%-13s', $tray->{'shelf'}), sprintf('%-4s', $tray->{'size'}), sprintf('%-12s', $tray->{'tray'}), sprintf('%5d', $tray->{'items'}), sprintf('%5d', $tray->{'width'}), sprintf('%7.2f', 100 * ($tray->{'width'} / $widths{$tray->{'twidth'}}))))."\n";
+            #~ $table .= "    <tr>\n".join ('', map("        <td>$_</td>\n", ($tray->{'shelf'}, $tray->{'size'}, $tray->{'tray'}, $tray->{'items'}, $tray->{'inches'}, 100 * ($tray->{'width'} / $widths{$tray->{'twidth'}}))))."    \n</tr>\n";
             $table .= "    <tr>\n";
-            $table .= "        <td>$tray{'shelf'}</td>\n";
-            $table .= "        <td style=\"text-align: center;\">$tray{'size'}</td>\n";
-            $table .= "        <td>$tray{'tray'}</td>\n";
-            $table .= "        <td style=\"text-align: right;\">$tray{'items'}</td>\n";
-            $table .= "        <td style=\"text-align: right;\">".sprintf('%1d', $tray{'width'})."</td>\n";
-            $table .= "        <td style=\"text-align: right;\">".sprintf('%.2f', 100 * ($tray{'width'} / ($tray{'size'} =~ /^E/ ? 92 : 140)))."</td>\n";
+            $table .= "        <td>$tray->{'shelf'}</td>\n";
+            $table .= "        <td style=\"text-align: center;\">$tray->{'size'}</td>\n";
+            $table .= "        <td>$tray->{'tray'}</td>\n";
+            $table .= "        <td style=\"text-align: right;\">$tray->{'items'}</td>\n";
+            $table .= "        <td style=\"text-align: right;\">".sprintf('%1d', $tray->{'width'})."</td>\n";
+            $table .= "        <td style=\"text-align: right;\">".sprintf('%.2f', 100 * ($tray->{'width'} / $widths{$tray->{'twidth'}}))."</td>\n";
             $table .= "    </tr>\n";
         } else {
-            $erpt .= join(' | ', (sprintf('%-40s', $tray{'shelf'}), sprintf('%-4s', $tray{'size'}), sprintf('%-40s', $tray{'tray'}), sprintf('%5d', $tray{'items'}), sprintf('%5d', $tray{'width'}), sprintf('%7.2f', 100 * ($tray{'width'} / ($tray{'size'} =~ /^E/ ? 92 : 140)))))."\n";
-            #~ $etable .= "    <tr>\n".join ('', map("        <td>$_</td>\n", ($tray{'shelf'}, $tray{'size'}, $tray{'tray'}, $tray{'items'}, $tray{'inches'}, 100 * ($tray{'width'} / ($tray{'size'} =~ /^E/ ? 92 : 140)))))."    </tr>\n";
+            $erpt .= join(' | ', (sprintf('%-40s', $tray->{'shelf'}), sprintf('%-4s', $tray->{'size'}), sprintf('%-40s', $tray->{'tray'}), sprintf('%5d', $tray->{'items'}), sprintf('%5d', $tray->{'width'}), sprintf('%7.2f', 100 * ($tray->{'width'} / $widths{$tray->{'twidth'}}))))."\n";
+            #~ $etable .= "    <tr>\n".join ('', map("        <td>$_</td>\n", ($tray->{'shelf'}, $tray->{'size'}, $tray->{'tray'}, $tray->{'items'}, $tray->{'inches'}, 100 * ($tray->{'width'} / $widths{$tray->{'twidth'}}))))."    </tr>\n";
             $etable .= "    <tr>\n";
-            $etable .= "        <td>$tray{'shelf'}</td>\n";
-            $etable .= "        <td style=\"text-align: center;\">$tray{'size'}</td>\n";
-            $etable .= "        <td>$tray{'tray'}</td>\n";
-            $etable .= "        <td style=\"text-align: right;\">$tray{'items'}</td>\n";
-            $etable .= "        <td style=\"text-align: right;\">".sprintf('%1d', $tray{'width'})."</td>\n";
-            $etable .= "        <td style=\"text-align: right;\">".sprintf('%.2f', 100 * ($tray{'width'} / ($tray{'size'} =~ /^E/ ? 92 : 140)))."</td>\n";
+            $etable .= "        <td>$tray->{'shelf'}</td>\n";
+            $etable .= "        <td style=\"text-align: center;\">$tray->{'size'}</td>\n";
+            $etable .= "        <td>$tray->{'tray'}</td>\n";
+            $etable .= "        <td style=\"text-align: right;\">$tray->{'items'}</td>\n";
+            $etable .= "        <td style=\"text-align: right;\">".sprintf('%1d', $tray->{'width'})."</td>\n";
+            $etable .= "        <td style=\"text-align: right;\">".sprintf('%.2f', 100 * ($tray->{'width'} / $widths{$tray->{'twidth'}}))."</td>\n";
             $etable .= "    </tr>\n";
         }
-        say $fh join(',', ($tray{'shelf'}, $tray{'size'}, $tray{'tray'}, $tray{'items'}, $tray{'width'}, $tray{'inches'}, 100 * ($tray{'width'} / ($tray{'size'} =~ /^E/ ? 92 : 140)), $tray{'avg'}, $tray{'max'}, $tray{'min'}));
+        say $fh join(',', ($tray->{'shelf'}, $tray->{'size'}, $tray->{'tray'}, $tray->{'items'}, $tray->{'width'}, $tray->{'inches'}, 100 * ($tray->{'width'} / $widths{$tray->{'twidth'}}), $tray->{'avg'}, $tray->{'max'}, $tray->{'min'}));
     }
 }
 close($fh);
