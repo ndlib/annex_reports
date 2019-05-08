ef55836a1ff1d275ea3f3244f63bfca893a44695
Justin Rittenhouse <jrittenh@nd.edu>
2019-05-08 16:11:41 -0400

    Add parameterized email addresses

--- reports.sh
+++ reports.sh
@@ -4,22 +4,24 @@
 
 export RPT_PATH=/global/soft/aleph/src/annex
 
+export EMAILS=${1:-annex-reports-list@nd.edu}
+
 cd $RPT_PATH
 
 if [[ $(date +%-H) -eq "8" ]]; then
     # Daily Jobs
     ## Regular Jobs
-    $RPT_PATH/workerStats.pl -e annex-reports-list@nd.edu -d $(date +%Y%m%d -d yesterday)
+    $RPT_PATH/workerStats.pl -e $EMAILS -d $(date +%Y%m%d -d "yesterday")
 
     if [[ $(date +%w) -eq "1" ]]; then
         # Weekly Jobs
-        $RPT_PATH/fillRequestRpt.pl -e annex-reports-list@nd.edu
-        $RPT_PATH/fillRequestRpt.pl -f 2015-08-24 -u $(date +%Y-%m-%d -d "last sunday") -e annex-reports-list@nd.edu
-        $RPT_PATH/shelfTrayCount.pl -e annex-reports-list@nd.edu
-        $RPT_PATH/shelfFillRpt.pl -e annex-reports-list@nd.edu
+        $RPT_PATH/fillRequestRpt.pl -e $EMAILS
+        $RPT_PATH/fillRequestRpt.pl -f 2015-08-24 -u $(date +%Y-%m-%d -d "last sunday") -e $EMAILS
+        $RPT_PATH/shelfTrayCount.pl -e $EMAILS
+        $RPT_PATH/shelfFillRpt.pl -e $EMAILS
     fi
     if [[ $(date +%-d) -eq "1" ]] && [[ $(expr $(date +%m) / 3) -eq $(expr $(date +%m) / 3) ]] 2> /dev/null; then
-        $RPT_PATH/aisleTrayCounts.pl -e annex-reports-list@nd.edu
-        $RPT_PATH/shelfTrayList.pl -e annex-reports-list@nd.edu
+        $RPT_PATH/aisleTrayCounts.pl -e $EMAILS
+        $RPT_PATH/shelfTrayList.pl -e $EMAILS
     fi
 fi
