8657c601904a8012fefa4bec9c21cf38e2c820eb
Justin Rittenhouse <jrittenh@nd.edu>
2019-05-08 16:07:58 -0400

    Move to double-bracket tests

--- reports.sh
+++ reports.sh
@@ -6,12 +6,12 @@ export RPT_PATH=/global/soft/aleph/src/annex
 
 cd $RPT_PATH
 
-if [ $(date +%H) = "08" ]; then
+if [[ $(date +%H) = "08" ]]; then
     # Daily Jobs
     ## Regular Jobs
     $RPT_PATH/workerStats.pl -e annex-reports-list@nd.edu -d $(date +%Y%m%d -d yesterday)
 
-    if [ $(date +%w) = "1" ]; then
+    if [[ $(date +%w) = "1" ]]; then
         # Weekly Jobs
         $RPT_PATH/fillRequestRpt.pl -e annex-reports-list@nd.edu
         $RPT_PATH/fillRequestRpt.pl -f 2015-08-24 -u $(date +%Y-%m-%d -d "last sunday") -e annex-reports-list@nd.edu
